7dd3691be11a148d81eba3b84a4ff44d33bf30d7
Aidan Macklen <aidancmacklen@gmail.com>
2023-10-30 23:43:04 -0400

    about json

--- projects/part4/about/script.js
+++ projects/part4/about/script.js
@@ -6,9 +6,6 @@ window.onload = () => {
     document.getElementById("hamburger").onclick = toggleNav;
 };
 
-// Update your script.js file
-
-// Function to fetch and display the image from the JSON file
 const displayImage = async () => {
     const jsonDataUrl = "https://aidanmacklen.github.io/json/data.json";
 
@@ -16,15 +13,12 @@ const displayImage = async () => {
         const response = await fetch(jsonDataUrl);
         const data = await response.json();
 
-        // Get the image filename from the JSON data
         const imageFilename = data.image;
 
-        // Create an image element
         const img = document.createElement("img");
         img.src = imageFilename;
         img.alt = "Selfie";
 
-        // Add the image to the 'img-container' div
         const imgContainer = document.getElementById("img-container");
         imgContainer.appendChild(img);
     } catch (error) {
@@ -32,7 +26,6 @@ const displayImage = async () => {
     }
 };
 
-// Call the function to display the image
 displayImage();
 
   
