bdcb97facfe8bc16c9af7d1ab77b2b5b3eb7e411
Aidan Macklen <aidancmacklen@gmail.com>
2023-10-30 23:53:05 -0400

    img json

--- projects/part4/home/script.js
+++ projects/part4/home/script.js
@@ -5,3 +5,25 @@ const toggleNav = () => {
 window.onload = () => {
     document.getElementById("hamburger").onclick = toggleNav;
 };
+
+const displayImage = async () => {
+    const jsonDataUrl = "https://aidanmacklen.github.io/json/data.json";
+
+    try {
+        const response = await fetch(jsonDataUrl);
+        const data = await response.json();
+
+        const imageFilename = data.image-home;
+
+        const img = document.createElement("img");
+        img.src = imageFilename;
+        img.alt = "Home";
+
+        const imgContainer = document.getElementById("image");
+        imgContainer.appendChild(img);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+displayImage();
