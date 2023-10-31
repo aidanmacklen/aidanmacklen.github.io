5ed82fa154809f26a412cc923d0738d625d678f0
Aidan Macklen <aidancmacklen@gmail.com>
2023-10-31 01:12:50 -0400

    iframe

--- projects/part4/skills/script.js
+++ projects/part4/skills/script.js
@@ -5,3 +5,24 @@ const toggleNav = () => {
 window.onload = () => {
     document.getElementById("hamburger").onclick = toggleNav;
 };
+
+const displayImage = async () => {
+    const jsonDataUrl = "https://aidanmacklen.github.io/json/data.json";
+
+    try {
+        const response = await fetch(jsonDataUrl);
+        const data = await response.json();
+
+        const imageFilename = data.iframe;
+
+        const img = document.createElement("iframe");
+        iframe.src = imageFilename;
+
+        const imgContainer = document.getElementById("image");
+        imgContainer.appendChild(img);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+displayImage();
