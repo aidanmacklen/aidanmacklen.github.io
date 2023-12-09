351218d4ed5030e06d87de443174143da59bcef7
Aidan Macklen <aidancmacklen@gmail.com>
2023-12-09 02:04:26 -0500

    minor change

---
+++ projects/part5/interactive/public/script.js
@@ -0,0 +1,165 @@
+const getJewels = async() => {
+    try {
+        return (await fetch("api/jewels/")).json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const showJewels = async() => {
+    let jewels = await getJewels();
+    let jewelsDiv = document.getElementById("jewel-list");
+    jewelsDiv.innerHTML = "";
+    jewels.forEach((jewel) => {
+        const section = document.createElement("section");
+        section.classList.add("jewel");
+        jewelsDiv.append(section);
+
+        const a = document.createElement("a");
+        a.href = "#";
+        section.append(a);
+
+        const h3 = document.createElement("h3");
+        h3.innerHTML = jewel.material;
+        a.append(h3);
+
+        const img = document.createElement("img");
+        img.src = jewel.img;
+        section.append(img);
+
+        a.onclick = (e) => {
+            e.preventDefault();
+            displayDetails(jewel);
+        };
+    });
+};
+
+const displayDetails = (jewel) => {
+    const jewelDetails = document.getElementById("jewel-information");
+    jewelDetails.innerHTML = "";
+
+    const h3 = document.createElement("h3");
+    h3.innerHTML = jewel.material;
+    jewelDetails.append(h3);
+
+    const dLink = document.createElement("a");
+    dLink.innerHTML = "	&#x2715;";
+    jewelDetails.append(dLink);
+    dLink.id = "deletel";
+
+    const eLink = document.createElement("a");
+    eLink.innerHTML = "&#9998;";
+    jewelDetails.append(eLink);
+    eLink.id = "editl";
+
+    const p = document.createElement("p");
+    jewelDetails.append(p);
+    p.innerHTML = `<strong>Size:</strong> ${jewel.size}`;
+
+    const p2 = document.createElement("p");
+    jewelDetails.append(p2);
+    p2.innerHTML = `<strong>Gem:</strong> ${jewel.gem}`;
+
+    eLink.onclick = (e) => {
+        e.preventDefault();
+        document.querySelector(".box-text").classList.remove("hide");
+        document.getElementById("ae-title").innerHTML = "Edit Jewel";
+    };
+    dLink.onclick = (e) => {
+        e.preventDefault();
+        const userConfirmed = window.confirm("Are you sure you want to delete this jewel?");
+        if (userConfirmed) {
+            deleteJewel(jewel);
+        } else {
+        console.log("Deletion canceled");
+        }
+    };
+
+    populateEditForm(jewel);
+};
+
+const deleteJewel = async(jewel) => {
+    let response = await fetch(`/api/jewels/${jewel._id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json;charset=utf-8"
+        }
+    });
+    if (response.status != 200) {
+        console.log("error deleting");
+        return;
+    }
+
+    let result = await response.json();
+    showJewels();
+    document.getElementById("jewel-information").innerHTML = "";
+    resetForm();
+}
+
+const populateEditForm = (jewel) => {
+    const form = document.getElementById("ae-jewel-form");
+    form._id.value = jewel._id;
+    form.material.value = jewel.material;
+    form.size.value = jewel.size;
+    form.gem.value = jewel.gem;
+};
+
+const addEditJewel = async(e) => {
+    e.preventDefault();
+    const form = document.getElementById("ae-jewel-form");
+    const formData = new FormData(form);
+    let response;
+    if (form._id.value == -1) {
+        formData.delete("_id");
+        response = await fetch("/api/jewels", {
+            method: "POST",
+            body: formData
+        });
+    }
+    else {
+        console.log(...formData);
+        response = await fetch(`/api/jewels/${form._id.value}`, {
+            method: "PUT",
+            body: formData
+        });
+    }
+    if (response.status != 200) {
+        console.log("Error posting data");
+    }
+    jewel = await response.json();
+    if (form._id.value != -1) {
+        displayDetails(jewel);
+    }
+
+    resetForm();
+    document.querySelector(".box-text").classList.add("hide");
+    showJewels();
+};
+
+const resetForm = () => {
+    const form = document.getElementById("ae-jewel-form");
+    form.reset();
+    form._id = "-1";
+};
+
+const showHideAdd = (e) => {
+    e.preventDefault();
+    document.querySelector(".box-text").classList.remove("hide");
+    document.getElementById("ae-title").innerHTML = "Add Jewel";
+    resetForm();
+};
+
+const toggleNav = () => {
+    document.getElementById("nav-items").classList.toggle("hide-small");
+};
+
+window.onload = () => {
+    showJewels();
+    document.getElementById("ae-jewel-form").onsubmit = addEditJewel;
+    document.getElementById("addl").onclick = showHideAdd;
+    document.querySelector(".close").onclick = () => {
+        document.querySelector(".box-text").classList.add("hide");
+    };
+    document.getElementById("addf").onclick = addFact;
+    document.getElementById("hamburger").onclick = toggleNav;
+};
---
+++ projects/part5/interactive/server.js
@@ -0,0 +1,113 @@
+const express = require("express");
+const app = express();
+const Joi = require("joi");
+const multer = require("multer");
+app.use(express.static("public"));
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+const mongoose = require("mongoose");
+
+const upload = multer({ dest: __dirname + "/public/images" }); 
+
+mongoose
+    .connect("mongodb+srv://aidancmacklen:Coolshoes107-@firstcluster.vvj7bt0.mongodb.net/?retryWrites=true&w=majority")
+    .then(()=>{console.log("Connected to mongodb")})
+    .catch((error)=>console.log("Couldn't connect to mongodb slime", error));
+
+const jewelSchema = new mongoose.Schema({
+    material:String,
+    size:String, 
+    gem:String,
+});
+
+const Jewel = mongoose.model("Jewel", jewelSchema);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html"); 
+});
+
+app.get("/api/jewels", (req, res) => {
+    getJewels(res);
+});
+
+const getJewels = async (res) => {
+    const jewels = await Jewel.find();
+    res.send(jewels);
+};
+
+app.post("/api/jewels", upload.single("img"), (req, res) => {
+    const result = validateJewel(req.body);
+    
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const jewel = new Jewel({
+        material:req.body.material,
+        size:req.body.size,
+        gem:req.body.gem
+    });
+
+    if (req.file) {
+        jewel.img = "images/" + req.file.filename;
+    }
+
+    createJewel(jewel, res);
+});
+
+const createJewel = async (jewel, res) => {
+    const result = await jewel.save();
+    res.send(jewel);
+}
+
+app.delete("/api/jewels/:id", upload.single("img"), (req, res) => {
+    removeJewels(res, req.params.id);
+});
+
+const removeJewels = async(res, id) => {
+    const jewel = await Jewel.findByIdAndDelete(id);
+    res.send(jewel);
+}
+
+app.put("/api/jewels/:id", upload.single("img"), (req, res) => {
+    const result = validateJewel(req.body);
+    
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    updateJewel(req, res);
+});
+
+const updateJewel = async (req, res) => {
+    let fieldsToUpdate = {
+        material:req.body.material,
+        size:req.body.size,
+        gem:req.body.gem
+    }
+
+    if(req.file) {
+        fieldsToUpdate.img - "images/" + req.file.filename;
+    }
+
+    const result = await Jewel.updateOne({_id:req.params.id}, fieldsToUpdate);
+    const jewel = await Jewel.findById(req.params.id);
+    res.send(jewel);
+};
+
+const validateJewel = (jewel) => {
+    const schema = Joi.object({
+        _id: Joi.allow(""),
+        material: Joi.string().min(3).required(),
+        size: Joi.string().min(3).required(),
+        gem: Joi.string().min(3).required()
+    });
+
+    return schema.validate(jewel);
+};
+
+app.listen(3000, () => {
+    console.log("On");
+});
